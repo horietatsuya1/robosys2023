dacde4b9a62340f48fad83d92640906cb6b80786
horietatsuya1 <s22C1117mc@s.chibakoudai.jp>
2023-11-28 10:27:14 +0900

    Update README

--- test.bash
+++ test.bash
@@ -1,23 +1,28 @@
-#!/bin/bash -xv
+#!/bin/bash -xv 
 # SPDX-FileCopyrightText: 2022 Ryuichi Ueda
 # SPDX-License-Identifier: BSD-3-Clause
 
-$ ./test.bash
-
+ng () {
+	echo NG at Line $1
+	res=1
+}
 res=0
-+ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-++ seq 5
-++ ./plus
-+ out=15
 [ "${out}" = 15 ] || ng ${LINENO}
-+ '[' 15 = 15 ']'
+
+### STRANGE ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+
+### ST
 
 [ "$res" = 0 ] && echo OK
-+ '[' 0 = 0 ']'
-+ echo OK
-OK
 exit $res
-+ exit 0
